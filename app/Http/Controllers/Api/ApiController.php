fb28d426ebe60fd9f3d04a2007fe9fd67a43b322
Nayana <nayanaphilip13@gmail.com>
2022-03-17 10:31:56 +0530

    Updated the folder url for BaseController

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Controllers\Api\BaseController as BaseController;
 use App\Models\Category;
 use App\Models\Menu;
 use App\Models\Table;
